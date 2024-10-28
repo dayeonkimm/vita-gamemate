b208c81ae5b67ee964442b0316ded644c151dc37
hwangtate <ghkdxodud10253504@gmail.com>
2024-10-28 13:10:34 +0900

    💡Chore: 주석제거

--- src/mates/serializers/mate_serializer.py
+++ src/mates/serializers/mate_serializer.py
@@ -41,10 +41,8 @@ class MateGameInfoSerializer(serializers.ModelSerializer):
         ]
 
     def get_review_count(self, obj):
-        # 특정 game_id에 대한 리뷰 개수를 반환
         return Review.objects.filter(game_request__game_id=obj.game_id).count()
 
     def get_average_rating(self, obj):
-        # 특정 game_id에 대한 평균 평점을 반환
         average = Review.objects.filter(game_request__game_id=obj.game_id).aggregate(Avg("rating"))["rating__avg"]
-        return round(average, 2) if average is not None else 0  # 리뷰가 없는 경우 0 반환
+        return round(average, 2) if average is not None else 0
