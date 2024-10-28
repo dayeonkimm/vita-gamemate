47b0b8b856d48b8c7bfcd2c78a6ed0212054bbc7
dayeonkimm <ekdyd516@gmail.com>
2024-10-28 10:11:20 +0900

    🔨Refactor: 채팅 모델 변경 및 api 수정

--- src/chats/models.py
+++ src/chats/models.py
@@ -5,18 +5,21 @@ User = get_user_model()
 
 
 class ChatRoom(models.Model):
-    main_user = models.ForeignKey(User, related_name="main_user", on_delete=models.CASCADE)
-    other_user = models.ForeignKey(User, related_name="other_user", on_delete=models.CASCADE)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
+class ChatRoomUser(models.Model):
+    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name="chatroom_users")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_chatrooms")
+
     class Meta:
-        unique_together = ("main_user", "other_user")
+        unique_together = ("chatroom", "user")
 
 
 class Message(models.Model):
     room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name="messages")
-    sender_nickname = models.CharField(max_length=10, null=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="message_sender")
     text = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- src/chats/serializers.py
+++ src/chats/serializers.py
@@ -15,9 +15,7 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     other_user_nickname = serializers.SerializerMethodField()
     other_user_id = serializers.SerializerMethodField()
     other_user_profile_image = serializers.SerializerMethodField()
-    # messages = MessageSerializer(many=True, read_only=True, source="messages.all")
     latest_message_time = serializers.SerializerMethodField()
-    updated_at = serializers.SerializerMethodField()
 
     class Meta:
         model = ChatRoom
@@ -32,34 +30,45 @@ class ChatRoomSerializer(serializers.ModelSerializer):
             "updated_at",
         )
 
-    # 최신 메시지를 가져오는 메소드
     def get_latest_message(self, obj):
-        latest_msg = Message.objects.filter(room=obj).order_by("-updated_at").first()
-        if latest_msg:
-            # latest_message와 last_message_time을 함께 반환
-            self.context["latest_message_time"] = latest_msg.updated_at
-            return latest_msg.text
-        return None
+        return self.context.get("latest_message")
 
-    # 최신 메시지의 시간을 가져오는 메소드
     def get_latest_message_time(self, obj):
         return self.context.get("latest_message_time")
 
-    # main_user의 닉네임을 반환하는 메소드
     def get_main_user_nickname(self, obj):
-        print("Object type11:", type(obj))
-        print("get_main_user_nickname called with obj:", obj)
-        return obj.main_user.nickname
+        return self.context["request"].user.nickname
 
-    # other_user의 닉네임을 반환하는 메소드
     def get_other_user_nickname(self, obj):
-        return obj.other_user.nickname
+        return self.context.get("other_user_data", {}).get("nickname")
 
     def get_other_user_id(self, obj):
-        return obj.other_user.id
+        return self.context.get("other_user_data", {}).get("id")
 
     def get_other_user_profile_image(self, obj):
-        return obj.other_user.profile_image.url if obj.other_user.profile_image else None
+        return self.context.get("other_user_data", {}).get("profile_image")
 
-    def get_updated_at(self, obj):
-        return obj.updated_at
+
+class ChatRoomListSerializer(serializers.ModelSerializer):
+    main_user_nickname = serializers.SerializerMethodField()
+    other_user_nickname = serializers.CharField()
+    other_user_id = serializers.IntegerField()
+    other_user_profile_image = serializers.URLField()
+    latest_message = serializers.CharField(allow_null=True)
+    latest_message_time = serializers.DateTimeField(allow_null=True)
+
+    class Meta:
+        model = ChatRoom
+        fields = (
+            "id",
+            "main_user_nickname",
+            "other_user_nickname",
+            "other_user_id",
+            "other_user_profile_image",
+            "latest_message",
+            "latest_message_time",
+            "updated_at",
+        )
+
+    def get_main_user_nickname(self, obj):
+        return self.context["request"].user.nickname
