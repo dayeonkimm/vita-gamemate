ced2e66372a94fbdece1f5a40e322519edeb1a22
hwangtate <ghkdxodud10253504@gmail.com>
2024-10-28 13:05:00 +0900

    🔨Refactor: 평균평점, 리뷰 수 필드 추가
    - method serializer로 추가함

--- src/mates/serializers/mate_serializer.py
+++ src/mates/serializers/mate_serializer.py
@@ -1,7 +1,9 @@
+from django.db.models.aggregates import Avg
 from rest_framework import serializers
 
 from games.models import Game
 from mates.models import MateGameInfo
+from reviews.models import Review
 
 
 class RegisterMateSerializer(serializers.ModelSerializer):
@@ -23,7 +25,26 @@ class RegisterMateSerializer(serializers.ModelSerializer):
 
 
 class MateGameInfoSerializer(serializers.ModelSerializer):
+    review_count = serializers.SerializerMethodField()
+    average_rating = serializers.SerializerMethodField()
 
     class Meta:
         model = MateGameInfo
-        fields = ["game_id", "description", "image", "level", "request_price"]
+        fields = [
+            "game_id",
+            "description",
+            "image",
+            "level",
+            "request_price",
+            "review_count",
+            "average_rating",
+        ]
+
+    def get_review_count(self, obj):
+        # 특정 game_id에 대한 리뷰 개수를 반환
+        return Review.objects.filter(game_request__game_id=obj.game_id).count()
+
+    def get_average_rating(self, obj):
+        # 특정 game_id에 대한 평균 평점을 반환
+        average = Review.objects.filter(game_request__game_id=obj.game_id).aggregate(Avg("rating"))["rating__avg"]
+        return round(average, 2) if average is not None else 0  # 리뷰가 없는 경우 0 반환
