332ce18cd61ff47df1350981221b31e96e04ba1f
dayeonkimm <ekdyd516@gmail.com>
2024-11-07 14:24:58 +0900

    🔨Refactor: 메세지 redis에 저장 후 주기적 동기화

--- src/chats/apps.py
+++ src/chats/apps.py
@@ -1,6 +1,20 @@
+import asyncio
+import threading
+
 from django.apps import AppConfig
 
 
 class ChatsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "chats"
+
+    def ready(self):
+        from .consumers import start_periodic_sync
+
+        def run_async_task():
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(start_periodic_sync())
+
+        thread = threading.Thread(target=run_async_task, daemon=True)
+        thread.start()
